4ecb664e6aac9ccadf634a84ac62a110768345ef
谢雷 <xl19880619@gmail.com>
2019-09-23 00:58:09 +0800

    add

--- lib/IncreaseTests.dart
+++ lib/IncreaseTests.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
 
-class Counter extends StatefulWidget{
+class CounterDisplay extends StatelessWidget {
+
+  CounterDisplay({this.count});
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return new RaisedButton(
+      onPressed: onPressed,
+      child: new Text('Increment'),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
   @override
   _CounterState createState() => new _CounterState();
 }
 
-class _CounterState extends State<Counter>{
-  
+class _CounterState extends State<Counter> {
   int _counter = 0;
 
-  void _increment(){
-    setState((){
-      _counter ++;
+  void _increment() {
+    setState(() {
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Row(
-      backgroundColor: Colors.white,
-      children: <Widget>[
-        new RaisedButton(
-          onPressed: _increment,
-          child: new Text('Increment'),
-        ),
-        new Text('Count: $_counter'),
-      ],
+    return new Material(
+      child: new Row(
+        children: <Widget>[
+          new CounterIncrementor(onPressed: _increment),
+          new CounterDisplay(count: _counter),
+        ],
+      ),
     );
   }
 }
